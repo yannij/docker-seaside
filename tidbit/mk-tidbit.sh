8977d65e01556148c0e455d633df9f7503ef9a99
CoreOS Admin <yanni.code@gmail.com>
2018-11-05 05:43:51 +0000

    Can build and deploy Material Design Components example

--- tidbit/mk-tidbit.sh
+++ tidbit/mk-tidbit.sh
@@ -2,14 +2,35 @@
 mkdir -p build
 cd build
 #curl https://get.pharo.org/64/ | bash
+
 cp -p Pharo.image tidbit.image
 cp -p Pharo.changes tidbit.changes
-./pharo tidbit.image eval --save "
+
+cat > load-tidbit.st <<EOS
 Metacello new
     githubUser: 'yannij' project: 'Tidbit' commitish: 'master' path: 'src';
     baseline: 'Tidbit';
-    onConflictUseIncoming;
+    onConflictUseLoaded;
     onWarningLog;
-    load.
-"
-./pharo tidbit.image eval --save "ZnZincServerAdaptor startOn: 8080"
+    load: #('default')
+EOS
+
+cat > configure.st <<EOS
+"Disable developer toolbar"
+WAAdmin applicationDefaults removeParent: WADevelopmentConfiguration instance.
+
+"Unregister all Seaside applications"
+WADispatcher default handlers keys do:[:name | WAAdmin unregister:name].
+
+"Register Seaside applications to be deployed"
+MCWBasicLayoutApp initialize.
+MCWDemoCatalogApp initialize.
+MCWExampleApplication initialize.
+MCWShrineApp initialize.
+
+"Start Seaside server on port 8080"
+ZnZincServerAdaptor startOn: 8080.
+EOS
+
+./pharo -vm-display-null -vm-sound-null tidbit.image eval --save load-tidbit.st
+./pharo -vm-display-null -vm-sound-null tidbit.image eval --save configure.st
